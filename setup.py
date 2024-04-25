f8d3b8fd9acd224fd42ce2fa21a19ccf82dd45d2
Javier Abad <79517685+jaabmar@users.noreply.github.com>
2024-04-25 21:53:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         "numpy==1.24.3",
         "scipy==1.10.1",
-        "mlinsights==0.5.0",
+        # "mlinsights==0.5.0",
         "scikit-learn==1.3.0",
         "pandas==1.5.3",
         "xgboost==1.7.3",
