7e9805090469bc10542905a1a9966174a8465340
Javier Abad <79517685+jaabmar@users.noreply.github.com>
2024-04-25 18:21:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         "numpy==1.24.3",
         "scipy==1.10.1",
-        "mlinsights==0.4.664",
+        "mlinsights==0.5.0",
         "scikit-learn==1.3.0",
         "statsmodels==0.13.5",
         "pandas==1.5.3",
